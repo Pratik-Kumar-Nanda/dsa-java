a9e1cecbcc5198a6ce4654a55656cb0b29f0a594
Pratik-Kumar-Nanda <pratik.kumar.nanda.official@gmail.com>
2026-07-20 23:53:47 +0530

    Time: 8 ms (68.44%), Space: 47.9 MB (45.53%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -2,26 +2,19 @@ class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int start = 1;
         int end = 0;
-        int ans = Integer.MAX_VALUE;
 
         for (int nums : piles)
         {
             end = Math.max(end, nums);
         }
 
-        if(piles.length == h)
-        {
-            return end;
-        }
-
         while(start <= end)
         {
             int mid = start + (end - start)/2;
-            int time = Total_time(piles, mid);
+            long time = Total_time(piles, mid);
 
             if(time <= h)
             {
-                ans = Math.min(ans,mid);
                 end = mid - 1;
             }
             else
@@ -29,16 +22,16 @@ class Solution {
                 start = mid + 1;
             }  
         }
-        return ans;
+        return start;
     }
 
-    private int Total_time(int[] piles, double k)
+    private long Total_time(int[] piles, int k)
     {
-        double hours = 0;
-        for(int banana : piles)
+        long hours = 0;
+        for (int banana : piles)
         {
-            hours += Math.ceil(banana/k);
+            hours += (banana + (long)k - 1) / k;
         }
-        return (int)hours;
+        return hours;
     }
 }
