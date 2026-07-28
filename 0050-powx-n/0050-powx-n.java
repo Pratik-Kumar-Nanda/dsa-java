3ebd969fe0d648e0f24de97b9107e21d75dde642
Pratik-Kumar-Nanda <pratik.kumar.nanda.official@gmail.com>
2026-07-28 23:02:56 +0530

    Time: 0 ms (100%), Space: 47.9 MB (65.12%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long exp = n;
+
+        if(exp < 0)
+        {
+            x = 1/x;
+            exp = -exp;
+        }
+
+        double ans = 1;
+
+        while (exp > 0)
+        {
+            if((exp & 1) == 1)
+            {
+                ans *= x;
+            }
+            x *= x;
+            exp /= 2;
+        }
+
+        return ans;
+    }
+}
